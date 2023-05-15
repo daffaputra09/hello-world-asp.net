ae96375d2d9716b0ea00a512db84a3859b251675
daffaputra09 <daffaputra863@gmail.com>
2023-05-15 09:35:20 +0700

    Add Tailwindcss

---
+++ HelloWorld/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './Pages/**/*.cshtml',
+        './Views/**/*.chstml'    ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
